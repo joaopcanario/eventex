6206b5593ed74af9a8ae7fa991402f9526d04256
João Paulo Canário <jopacanario@gmail.com>
2017-08-11 18:05:56 -0300

    Create model subscription

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+
+    def setUp(self):
+        self.obj = Subscription(
+            name='Joao Paulo Canario',
+            cpf='12345678901',
+            email='jopacanario@gmail.com',
+            phone='71-999538382'
+        )
+
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        '''Subscription must have an auto created_at attribute'''
+
+        self.assertIsInstance(self.obj.created_at, datetime)
